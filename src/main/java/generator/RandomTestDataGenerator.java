3de7a6f13425ab147f824f0a40b92be40d2786c1
Dennis <joklin@live.dk>
2018-10-02 13:43:16 +0200

    datagen

--- src/main/java/generator/RandomTestDataGenerator.java
+++ src/main/java/generator/RandomTestDataGenerator.java
@@ -16,12 +16,14 @@ public class RandomTestDataGenerator {
         String[] firstNames = { "Benjamin" , "Merle" , "Nikolaj" , "Marie" , "Ellen" , "Arthur" , "Karla" , "Matias" , "Marius" , "Marcus" , "Sofia" , "Villads" , "Marcus" , "Simon" , "William" , "Maja" , "Jonathan" , "Mads" , "Ida" , "Jonathan" , "Magnus" , "Jakob" , "Lucas" , "Nicoline" , "Julie" , "August" , "Nicoline" , "Laura" , "Thea" , "Mille" };
         String[] lastNames = { "Jørgensen" , "Pedersen" , "Sørensen" , "Nielsen" , "Christensen" , "Poulsen" , "Vad" , "Jensen" , "Hansen" , "Larsen" , "Mortensen" , "Andersen" , "Kristensen" , "Thomsen" , "Møller" , "Christiansen" , "Madsen" , "Petersen"  };
         String[] address = {"Lærkevej" , "Birkevej" , "Vibevej" , "Vinkelvej" , "Østergade" , "Engvej" , "Vestergade" , "Møllevej" , "Kirkevej" , "Bøgevej" , "Industrivej" , "Tværvej" , "Stationsvej" , "Elmevej" , "Skovvej" , "Nørregade" , "Bakkevej" , "Søndergade" , "Skolevej" , "Drosselvej" , "Fasanvej"};
-        Random rndAddressNumber = new Random();
+        Random rnd = new Random();
         Random rndPhoneNumber = new Random();
         
         for (int i = 0; i < samples; i++) {
         Person p = new Person();
-        
+        p.setFirstName(firstNames[rnd.nextInt(firstNames.length) + 1]);
+        p.setLastName(lastNames[rnd.nextInt(lastNames.length) + 1]);
+        p.setCity(city);
             result.add(p);
         }
            return "";     
