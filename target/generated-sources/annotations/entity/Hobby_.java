9032a417d41883b165607d965a7ed15f7658d781
Dennis <joklin@live.dk>
2018-10-03 10:29:59 +0200

    gitignor

---
+++ target/generated-sources/annotations/entity/Hobby_.java
@@ -0,0 +1,17 @@
+package entity;
+
+import entity.Person;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-03T10:27:36")
+@StaticMetamodel(Hobby.class)
+public class Hobby_ { 
+
+    public static volatile ListAttribute<Hobby, Person> hobbys;
+    public static volatile SingularAttribute<Hobby, String> name;
+    public static volatile SingularAttribute<Hobby, String> description;
+
+}
