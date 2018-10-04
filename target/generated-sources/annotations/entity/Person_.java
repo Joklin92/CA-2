07804b3b6552bd18c8cd1040f8b4b0ed3c8e021e
Dennis <joklin@live.dk>
2018-10-04 09:31:39 +0200

    testdatagen working?

--- target/generated-sources/annotations/entity/Person_.java
+++ target/generated-sources/annotations/entity/Person_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-04T09:03:46")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-04T09:20:30")
 @StaticMetamodel(Person.class)
 public class Person_ { 
 
