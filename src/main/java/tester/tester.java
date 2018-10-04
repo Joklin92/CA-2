9d8649cf062274a40722114c8ab5070f5ebd2b01
Jonas Elbro <cph-je98@cphbusiness.dk>
2018-10-04 10:51:59 +0200

    fix merge

--- src/main/java/tester/tester.java
+++ src/main/java/tester/tester.java
@@ -17,13 +17,10 @@ public class tester {
 
         //  System.out.println(facade.getZipCodes());
         //  System.out.println(facade.getPersonByPhone(73463229));
-<<<<<<< HEAD
         //System.out.println(facade.getallcompletePersons());        
         // System.out.println(facade.getPersonsByCity(facade.getAddressByZip(1903)));
-=======
         //System.out.println(facade.getAllPersons());        
         //System.out.println(facade.getPersonsByCity(1903));
->>>>>>> ce469df43b87296305d551169f2c70d11ff14be5
         //System.out.println(facade.getAddressByZip(1903));
     }
 
